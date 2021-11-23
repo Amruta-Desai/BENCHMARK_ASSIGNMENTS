db79b523accf7045f8c42f7e1c30d60b272c9b05
Amruta-Desai <70656972+Amruta-Desai@users.noreply.github.com>
2021-11-23 20:16:26 +0530

    Add files via upload

---
+++ Assignment 1/fibonnaciSeries/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonnaciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, sum=1, n;
+            Console.Write("Enter the number upto which fibonnaci series is to be found? : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(sum <= n)
+            {
+                Console.WriteLine(sum + " ");
+                n1 = n2;
+                n2 = sum;
+                sum = n1 + n2;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}

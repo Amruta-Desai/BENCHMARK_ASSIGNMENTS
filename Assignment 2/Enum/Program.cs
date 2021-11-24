02b093e5cbb757069fef5e41cb1b8d52d12c7960
Amruta-Desai <70656972+Amruta-Desai@users.noreply.github.com>
2021-11-24 13:09:36 +0530

    Add files via upload

---
+++ Assignment 2/Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    internal class Program
+    {
+        public enum Days { Sun,Mon,Tue,Wed,Thur,Fri,Sat}
+        static void Main(string[] args)
+        {
+            Program p  =new Program();
+            p.PrintData(Days.Mon);
+            p.PrintData(Days.Wed);
+            p.PrintData(Days.Fri);
+
+            Console.ReadKey();
+
+        }
+        public void PrintData(Days d)
+        {
+            Console.WriteLine("The day is "+ d);
+         
+        }
+        
+    }
+    
+}

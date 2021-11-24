354366cf4cf0ded68eb6c1ec00e373642503d7e2
Amruta-Desai <70656972+Amruta-Desai@users.noreply.github.com>
2021-11-24 19:17:06 +0530

    Add files via upload

---
+++ Assignment 2/AbstractClasses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAnimal
+{
+    abstract class Animal
+    {
+
+        public abstract void foodHabit();
+
+    }
+
+
+    class Herbivorous : Animal
+    {
+
+        public override void foodHabit()
+        {
+            Console.WriteLine("Herbivorous animal Consume only plants");
+
+        }
+    }
+    class Carnivorous : Animal
+    {
+
+        public override void foodHabit()
+        {
+            Console.WriteLine("Carnivorous animals Consume meat");
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Herbivorous animals ");
+            new Herbivorous().foodHabit();
+            
+            Console.WriteLine("Carnivorous animals ");
+            new Carnivorous().foodHabit();
+            Console.ReadKey();
+        }
+    }
+}

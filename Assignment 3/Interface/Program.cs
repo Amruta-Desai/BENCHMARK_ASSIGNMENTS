49d98d316d26258857ae4caedcd56f787ab168a1
Amruta-Desai <70656972+Amruta-Desai@users.noreply.github.com>
2021-11-28 22:14:58 +0530

    Add files via upload

---
+++ Assignment 3/Interface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IPrintable
+    {
+        void print();
+    }
+    class Account : IPrintable
+    {
+        string acc = "Saving";
+        public void print()
+        {
+            Console.WriteLine("Print method- Account Class");
+            
+        }
+    }
+    class Employee : IPrintable
+    {
+        string Emp = "Permanent";
+        public void print()
+        {
+            Console.WriteLine("Print method- Employee Class");
+            
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Account().print();
+            new Employee().print();
+            Console.ReadKey();
+        }
+    }
+}

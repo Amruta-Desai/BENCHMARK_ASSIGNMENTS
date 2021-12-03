3baeb7bfa34376b4f932477f939b6e06ed9a98ce
Amruta-Desai <70656972+Amruta-Desai@users.noreply.github.com>
2021-12-03 09:38:03 +0530

    Add files via upload

---
+++ Assignment 4/FileStream/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Filestream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream F = new FileStream("D:\\Benchmark\\sample.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                for (int i = 1; i <= 20; i++)
+                {
+                    F.WriteByte((byte)i);
+                }
+                F.Position = 0;
+                for (int i = 0; i <= 20; i++)
+                {
+                    Console.Write(F.ReadByte() + " ");
+                }
+                F.Close();
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

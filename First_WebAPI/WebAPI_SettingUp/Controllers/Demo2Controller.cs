a1d33cf5dbc7029e32cc24bdcb635bca2af0a524
Amruta-Desai <70656972+Amruta-Desai@users.noreply.github.com>
2021-12-06 15:33:11 +0530

    Add files via upload

---
+++ First_WebAPI/WebAPI_SettingUp/Controllers/Demo2Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_API_SettingUp.Controllers
+{
+    public class Demo2Controller : ApiController
+    {
+        //public string get()
+        //{
+        //    return "Welcome to my first Web API";
+        //}
+
+        public List<string> get()
+        {
+            List<string> datalist = new List<string>();
+            datalist.Add("data1");
+            datalist.Add("data1");
+            return datalist;    
+        }
+    }
+}
